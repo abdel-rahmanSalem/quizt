02ca66b804604061a2ef42c940c48105eb9fd51a
Abderlahman Salem <abdelrahmansalemfahmy@gmail.com>
2024-07-11 17:20:33 +0300

    Fin: HomeSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
-// import HomePageHeader from "../components/HomePageHeader";
 import HeroSection from "../ui/HeroSection";
 // import Instructions from "../components/Instructions";
 
 function Home() {
   return (
     <div className="max-w-7xl mx-auto pt-20 px-6 text-white">
-      {/* <HomePageHeader /> */}
       <HeroSection />
       {/* <Instructions /> */}
     </div>
--- src/ui/HeroSection.jsx
+++ src/ui/HeroSection.jsx
@@ -3,14 +3,14 @@ import Button from "./ForwardButton";
 
 function HeroSection() {
   return (
-    <div className="flex flex-col items-center justify-center my-24">
+    <div className="flex flex-col items-center justify-center my-20">
       <h1 className="text-4xl sm:text-5xl lg:text-7xl text-center tracking-wide">
-        Your Only <span className="text-green-500">Quiz Hub</span>!
+        Your Only <span className="text-blue-400">Quiz Hub</span>.
       </h1>
       <p className="mt-8 mx-2 text-base lg:text-lg text-center text-neutral-400 max-w-2xl font-medium">
-        your one-stop hub for all things quiz-related. Easily create and host
+        A one-stop hub for all things quiz-related. Easily create and host
         quizzes or participate in existing ones. Join us and transform your
-        quizzing experience!
+        quizzing experience now!
       </p>
       <div className="flex justify-center items-center gap-8 mt-10">
         <Link to="/quiz-id">
