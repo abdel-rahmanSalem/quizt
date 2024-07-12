24f8d8604ec167c751e1b4733748a24575f7f9e9
Abderlahman Salem <abdelrahmansalemfahmy@gmail.com>
2024-07-12 12:46:35 +0300

    Typo: importing Navbar

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
-import Navbar from "./Navbar";
+import Navbar from "./navbar/Navbar";
 
 function AppLayout() {
   return (
