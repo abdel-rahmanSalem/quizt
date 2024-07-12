e73d80d54bdbc4a839fbc5d66f719ff76a9e8e98
Abderlahman Salem <abdelrahmansalemfahmy@gmail.com>
2024-07-12 19:51:31 +0300

    Fix: responsivity

---
+++ src/constants/shapes.js
@@ -0,0 +1,46 @@
+export const shapes = [
+  {
+    id: 1,
+    size: "w-20 h-20",
+    color: "bg-blue-400",
+    delay: 0.2,
+    style: "rounded-full",
+  },
+  {
+    id: 2,
+    size: "w-0 h-0",
+    color: "",
+    delay: 0.4,
+    style:
+      "border-solid border-x-[1.75rem] border-x-transparent border-t-[3.5rem] border-t-red-400",
+  },
+  {
+    id: 3,
+    size: "w-20 h-20",
+    color: "bg-yellow-400",
+    delay: 0.6,
+    style: "",
+  },
+  {
+    id: 4,
+    size: "w-24 h-24",
+    color: "bg-green-400",
+    delay: 0.8,
+    style: "rounded-full",
+  },
+  {
+    id: 5,
+    size: "",
+    color: "",
+    delay: 1.0,
+    style:
+      "w-0 h-0 border-solid border-x-[1.25rem] border-x-transparent border-b-[2.5rem] border-b-purple-400",
+  },
+  {
+    id: 6,
+    size: "w-16 h-16",
+    color: "bg-pink-400",
+    delay: 1.2,
+    style: "",
+  },
+];
